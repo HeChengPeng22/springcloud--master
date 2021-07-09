82dcc5cff2efdd34e7879da20a895136a47aecba
hechengpeng22 <2582721513@qq.com>
2021-07-09 19:30:38 +0800

    继续整合Hystrix

--- service-a/src/main/java/com/example/servicea/ServiceAApplication.java
+++ service-a/src/main/java/com/example/servicea/ServiceAApplication.java
@@ -3,10 +3,12 @@ package com.example.servicea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 //开启Eureka服务
 @EnableEurekaClient
+@EnableHystrix
 public class ServiceAApplication {
 
     public static void main(String[] args) {
--- service-a/src/main/java/com/example/servicea/controller/TestController.java
+++ service-a/src/main/java/com/example/servicea/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.servicea.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Value("${server.port}")
+    private String port;
+    @HystrixCommand(fallbackMethod = "fallback")
     @RequestMapping("testA")
     public String test01(){
-        return "Hello,Spring Cloud for Test";
+        try {
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return "Hello,Spring Cloud for Test"+port;
+    }
+
+    @RequestMapping("/otherService")
+    public String otherService(){
+        return "我是其他服务";
+    }
+    String fallback(){
+        return "服务器忙";
     }
 }
